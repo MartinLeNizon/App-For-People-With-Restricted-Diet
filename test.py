3fc1823d044ebfc59877b934eca1cb4d93bdcacf
MartinLeNizon <nizonmartin@gmail.com>
2024-02-07 10:37:42 +0000

    Function added for easier use.

--- test.py
+++ test.py
@@ -6,27 +6,51 @@ from transformers import CLIPProcessor, CLIPModel
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-image = Image.open("App-For-People-With-Restricted-Diet/dataset/003.jpg")
+def predict(image, allergen):
+    image = Image.open(image)
+    match allergen:
+        case 1:
+            inputs = processor(text=["a product containing wheat or gluten", "a product without wheat nor gluten"], images=image, return_tensors="pt", padding=True)
+        case 2:
+            inputs = processor(text=["a product containing eggs", "a product without eggs"], images=image, return_tensors="pt", padding=True)
+        case 3:
+            inputs = processor(text=["a product containing milk", "a product without milk"], images=image, return_tensors="pt", padding=True)
+        case 4:
+            inputs = processor(text=["a product containing nuts", "a product without nuts"], images=image, return_tensors="pt", padding=True)
+        case 5:
+            inputs = processor(text=["a product containing peanuts", "a product without peanuts"], images=image, return_tensors="pt", padding=True)
+        case 6:
+            inputs = processor(text=["a product containing soya", "a product without soya"], images=image, return_tensors="pt", padding=True)
+        case 7:
+            inputs = processor(text=["a product containing molluscs", "a product without molluscs"], images=image, return_tensors="pt", padding=True)
+        case 8:
+            inputs = processor(text=["a product containing fish", "a product without fish"], images=image, return_tensors="pt", padding=True)
+        case 9:
+            inputs = processor(text=["a product containing lupin", "a product without lupin"], images=image, return_tensors="pt", padding=True)
+        case 10:
+            inputs = processor(text=["a product containing crustaceans", "a product without crustaceans"], images=image, return_tensors="pt", padding=True)
+        case 11:
+            inputs = processor(text=["a product containing sesame", "a product without sesame"], images=image, return_tensors="pt", padding=True)
+        case 12:
+            inputs = processor(text=["a product containing mustard", "a product without mustard"], images=image, return_tensors="pt", padding=True)
+        case 13:
+            inputs = processor(text=["a product containing celery", "a product without celery"], images=image, return_tensors="pt", padding=True)
+        case 14:
+            inputs = processor(text=["a product containing sulphites", "a product without sulphites"], images=image, return_tensors="pt", padding=True)
 
-inputs = processor(text=["a product containing wheat or gluten", "a product without wheat nor gluten"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing eggs", "a product without eggs"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing milk", "a product without milk"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing nuts", "a product without nuts"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing peanuts", "a product without peanuts"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing soya", "a product without soya"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing molluscs", "a product without molluscs"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing fish", "a product without fish"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing lupin", "a product without lupin"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing crustaceans", "a product without crustaceans"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing sesame", "a product without sesame"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing mustard", "a product without mustard"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing celery", "a product without celery"], images=image, return_tensors="pt", padding=True)
-# inputs = processor(text=["a product containing sulphites", "a product without sulphites"], images=image, return_tensors="pt", padding=True)
+    outputs = model(**inputs)
+    logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
+    probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
+    
+    if probs[0][0] > 0.5:
+        return 1
+
+    return 0
 
 
 
-outputs = model(**inputs)
-logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
-probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
-print(logits_per_image)
-print(probs)
+def main():
+    print(predict("dataset/003.jpg", 1))    # First arg: image path; Second: allergen (1: gluten...)
+
+if __name__ == "__main__":
+    main()
