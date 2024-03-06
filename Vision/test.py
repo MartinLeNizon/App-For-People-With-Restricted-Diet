cc13d58b83285eeafb37e2e881898909235a0238
MartinLeNizon <nizonmartin@gmail.com>
2024-03-06 11:45:52 +0000

    Notes added.

--- Vision/test.py
+++ Vision/test.py
@@ -33,7 +33,8 @@ payload = {
         {
           "type": "image_url",
           "image_url": {
-            "url": f"data:image/jpeg;base64,{base64_image}"
+            "url": f"data:image/jpeg;base64,{base64_image}",
+            "detail": "auto"
           }
         }
       ]
